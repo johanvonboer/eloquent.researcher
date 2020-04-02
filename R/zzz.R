57ece096aae06b3f93c15a4c65651930ab3ccd40
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2020-04-02 15:03:37 +0200

    En grundläggande struktur för att sätta upp en databas-handle till projektets Emu-databas.
    * Databasen förutsätts ligga i ../Data och heta samma sak som biblioteket ett steg upp  i heirarkin (alltså, i det bibkliotek som Data, Dokument, Program och Resultat ligger.
    
    * En matchande onUnLoad / onDetach finns också, som kopplar ner databasen och tar bort den globala handlern

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,28 @@
 .onLoad <- function(libname, pkgname) {
-  assign("DATABASE", file.path(getwd(),
-                               "..",
-                               "Data",
-                               paste0("teet",emuR:::emuDB.suffix))
-         , envir = .GlobalEnv)
 
+  split_path <- function(path) {
+    if (dirname(path) %in% c(".", path)) return(basename(path))
+    return(c(basename(path), split_path(dirname(path))))
+  }
+  # Första elementet är "Program", och därefter är elementen bibliotek från / i omvänd ordning
+  project.name <- split_path(getwd())[2]
+  dataPath <- file.path(getwd(),
+                        "..",
+                        "Data")
+  if(dir.exists(dataPath)){
+    emuDBpath <- file.path(dataPath,
+                           paste0(project.name,emuR:::emuDB.suffix))
+
+    assign("EMUDB",emuR::load_emuDB(emuDBpath,verbose = FALSE), envir = .GlobalEnv)
+  }
 }
 
 .onUnLoad <- function(libname, pkgname) {
-  remove(list=c("DATABASE"),envir = .GlobalEnv)
+  if(exists("EMUDB")){
+    RSQLite::dbDisconnect(EMUDB$connection)
+    remove(list=c("EMUDB"),envir = .GlobalEnv)
+  }
+
 }
 
 .onDetach <- .onUnLoad

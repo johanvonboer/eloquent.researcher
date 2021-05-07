8585e2d60252f1f10acbf00b1b7f9f6ecc2a291e
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2021-05-07 08:28:14 +0200

    Revised the setup of database handles and base directories so that they fit with the current infrastructure setup
    DATA= poting to the data directory
    DB= the EmuDB database handle pointing to DATA/humlabspeech_emuDB
    RESULTS= poting to the Results directory
     Fixes #1

--- R/zzz.R
+++ R/zzz.R
@@ -1,46 +1,53 @@
 .onLoad <- function(libname, pkgname) {
+  #Handle the dual conventions used at humlab speech
+  pdir <- dir(pattern="*-project")
 
-  split_path <- function(path) {
-    if (dirname(path) %in% c(".", path)) return(basename(path))
-    return(c(basename(path), split_path(dirname(path))))
+  if(! length(pdir) ==  1 ){
+    baseDir <- "humlabspeech"
+  }else{
+    baseDir <- pdir
   }
-  # Första elementet är "Program", och därefter är elementen bibliotek från / i omvänd ordning
-  project.name <- split_path(getwd())[2]
-  dataPath <- file.path(getwd(),
-                        "..",
-                        "Data")
-  if(dir.exists(dataPath)){
-    emuDBpath <- file.path(dataPath,
-                           paste0(project.name,emuR:::emuDB.suffix))
-    if(dir.exists(emuDBpath)){
-      assign("EMUDB",emuR::load_emuDB(emuDBpath,verbose = FALSE), envir = .GlobalEnv)
-
-      if(git2r::in_repository(EMUDB$basePath) | length(git2r::branches(EMUDB$basePath))){
-        #Make sure that the database is current by
-        #checking out the master branch
-        git2r::checkout(path=emuDBpath)
-      }
-
+  if(!dir.exists(baseDir)){
+    pathArr <- strsplit(getwd(),.Platform$file.sep)[[1]]
+    baseInd <- match(baseDir,pathArr)
+    if(! is.na(baseInd)){
+        #set up HS reference
+        ind <- seq(1,baseInd,1)
+        assign("HS",file.path(pathArr[ind]))
 
     }else{
-      warning("The database \'",emuDBpath,"\' does not exist.\n You will not have EMUDB global variable available to you in the session.")
+      warning("Unable to set up pointers to HS, DB, DATA and RESULTS.")
     }
-  }
-  #Sätt up en
-  resultsPath <- file.path(getwd(),
-                          "..",
-                          "Resultat")
-  if(dir.exists(resultsPath)){
-    assign("RESULTAT",resultsPath)
   }else{
-    warning("The directory \'",resultsPath,"\' does not exist.\n You will not have RESULTAT global variable available to you in the session.")
+    #Make HS available as a reference to the "humlabspeech" directory globaly
+    assign("HS",file.path(getwd(),baseDir))
+    if(dir.exists(file.path(HS,"Data"))){
+      assign("DATA",file.path(HS,"Data"),envir = .GlobalEnv)
+      if(dir.exists(file.path(DATA,"humlabspeech_emuDB"))){
+        assign("DB",emuR::load_emuDB(file.path(DATA,"humlabspeech_emuDB")),envir = .GlobalEnv)
+      }else{
+        warnings("Could not create a DB reference to the speech database")
+      }
+    }else{
+      warnings("Could not create a DATA reference to the project Data directory")
+    }
+    if(dir.exists(file.path(HS,"Results"))){
+      assign("RESULTS",file.path(HS,"Results"),envir = .GlobalEnv)
+    }else{
+      warnings("Could not create a RESULTS reference to the project Results directory")
+    }
+    if(dir.exists(file.path(HS,"Applications"))){
+      assign("PROGRAMS",file.path(HS,"Programs"),envir = .GlobalEnv)
+    }else{
+      warnings("Could not create a PROGRAMS reference to the project Programs directory")
+    }
   }
 }
 
 .onUnLoad <- function(libname, pkgname) {
-  if(exists("EMUDB")){
+  if(exists("DB")){
     RSQLite::dbDisconnect(EMUDB$connection)
-    remove(list=c("EMUDB"),envir = .GlobalEnv)
+    remove(list=c("DB","RESULTS","DATA","HS"),envir = .GlobalEnv)
   }
 
 }
